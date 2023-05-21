ee9cb750a3d8ce49ffab9c549533fb5661930211
Nicholas264 <22565232+Nicholas264@users.noreply.github.com>
2023-05-20 18:59:30 -0700

    Add TextBubble.js

---
+++ src/js/TextBubble.js
@@ -0,0 +1,24 @@
+
+/**
+ * TextBubble Element:
+ * <text-bubble>
+ *     <div class="speaker"></div>
+ *     <div class="words"></div>
+ *     <div class="interactive">
+ *         <button></button>
+ *         <input type="text">
+ *     </div>
+ *     <div class="next-arrow"></div> 
+ * </text-bubble>
+ */
+
+class TextBubble extends HtmlElement {
+	constructor() {
+		super();
+	}
+
+	set data(data) {
+		if (!data) return;
+
+	}
+}
