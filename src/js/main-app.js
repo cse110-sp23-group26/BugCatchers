1db93453710c97f4d9e2c14359a1b899fe88e3e1
Shishir Iyer <shiyer@ucsd.edu>
2023-05-23 15:45:18 -0700

    a few tweaks to the fortune card element (styling TBD)

---
+++ src/js/main-app.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    const fortuneCard = document.createElement("fortune-card");
+    fortuneCard.data = {
+        "name": "adgjflkajgkl",
+        "data": "agjlkhg99ujj",
+        "text": "djfgliasud89o ljf jiasduotg8ayygyilhsadf"
+    }
+
+    document.querySelector("body").appendChild(fortuneCard);
+}
