38fee2e3cd0350260b7da76e07b649061c4e7a09
ruili4UCSD <rul043@ucsd.edu>
2023-05-28 15:54:22 -0700

    fixed the delete button

--- src/js/FortuneCard.js
+++ src/js/FortuneCard.js
@@ -1,8 +1,20 @@
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// Rui's edit 28/05/23:
+// changed the variable name div to fortuneCardContent
+// changed the innerhtml staff to create element and append it to fortuneCardContent
+// add the delete button funtion:
+//     when delete clicked:
+//     delete the element in localstorage with same id
+//     remove fortune card from html
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 class FortuneCard extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({ mode: "open" });
-        const div = document.createElement("div");
+        const fortuneCardContent = document.createElement("div");
         const style = document.createElement("style");
         style.innerHTML = `
         div {
@@ -22,12 +34,12 @@ class FortuneCard extends HTMLElement {
             bottom: 8px;
             right: 16px;
             font-size: 18px;
-          }
+        }
         `;
         shadow.appendChild(style);
-        shadow.appendChild(div);
+        shadow.appendChild(fortuneCardContent);
     }
-        
+
     /**
      * Passed in as JSON format as follows:
      * 
@@ -39,16 +51,39 @@ class FortuneCard extends HTMLElement {
      * }
      */
     set data(data) {
-        if(!data) return;
+        if (!data) return;
 
-        const div = this.shadowRoot.querySelector("div");
-        div.innerHTML = `
-            <p class="fc-text">${data.text}</p><br><br>
-            <p class="fc-name">${data.name}</p>
-            <button class="fc-delete">
-            DELETE
-            </button>
-        `;
+        const fortuneCardContent = this.shadowRoot.querySelector("div");
+
+        const textParagraph = document.createElement("p");
+        textParagraph.classList.add("fc-text");
+        textParagraph.textContent = data.text;
+
+        const nameParagraph = document.createElement("p");
+        nameParagraph.classList.add("fc-name");
+        nameParagraph.textContent = data.name;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("fc-delete");
+        deleteButton.textContent = "DELETE";
+
+        fortuneCardContent.append(textParagraph, nameParagraph, deleteButton);
+
+        deleteButton.addEventListener('click', (event) => {
+            // get the ancestor element fortune-card where the click happened
+            // and record the id
+            console.log(event.target);
+            const fortuneCard = event.target.getRootNode().host;
+            const id = data.id;
+
+            // delete the corresponding data from localStorage
+            let fortunes = JSON.parse(localStorage.getItem('FortunesCard'));
+            fortunes = fortunes.filter(fortune => fortune.id !== id);
+            localStorage.setItem('FortunesCard', JSON.stringify(fortunes));
+
+            // delete the fortune card from html
+            fortuneCard.remove();
+        });
     }
 }
 
