9bb6923ae2c75224b19a9064a066e75cf3f33bfa
ruili4UCSD <rul043@ucsd.edu>
2023-05-28 16:50:16 -0700

    added check in system

--- src/js/main-app.js
+++ src/js/main-app.js
@@ -9,11 +9,9 @@
 
 
 
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-
     // set those two data to local storage
     const fortunes = [
         {
@@ -29,21 +27,32 @@ function init() {
     ]
     localStorage.setItem('FortunesCard', JSON.stringify(fortunes));
 
+    // create fortune card and append it to 
     const fortuneCard = document.createElement("fortune-card");
     fortuneCard.data = {
         "name": "Celeste",
         "id": "fortune1",
         "text": "you will not die soon probably"
     }
-
     const fortuneCard2 = document.createElement("fortune-card");
     fortuneCard2.data = {
         "name": "Celeste",
         "id": "fortune2",
         "text": "you will literally explode"
     }
-
     const fortuneCardList = document.querySelector(".fortuneCardList");
     fortuneCardList.appendChild(fortuneCard);
     fortuneCardList.appendChild(fortuneCard2);
+
+    // init check-in system
+    // this fcuntion in js/util/CheckIn.js
+    initializeCheckIn();
+    // add event listener to check in button
+    // this fcuntion in js/util/CheckIn.js
+    document.getElementById('check-in-button').addEventListener('click', function() {
+        let result = checkIn();
+        // Update the display
+        document.getElementById('last-check-in-date').textContent = 'Last Check-In: ' + result.date;
+        document.getElementById('check-in-count').textContent = 'Count: ' + result.count;
+    });
 }
