053fee25db6627c55cbd2e1ef568475776619e27
Jianming <Georgegeng00@hotmail.com>
2023-05-30 00:43:32 -0700

    Fix some bugs up to predictions page

--- src/js/util/GetUserInput.js
+++ src/js/util/GetUserInput.js
@@ -57,92 +57,125 @@ function boxInit() {
     });
 }
 
+/**
+ * Check the input of day from user
+ */
 function dayCheck() {
     const boxInput = document.querySelector('#dataInput');
-    let userInputArray = [];
-    
-    // answer box initialize
-    boxInput.placeholder = 'mm';
 
     // check validity
     const pattern = /^(0[1-9]|[1-2][0-9]|3[0-1])$/
-    boxInput.addEventListener('input', inputCheck(/^(0[1-9]|[1-2][0-9]|3[0-1])$/));
+    boxInput.addEventListener('input', inputCheck(pattern));
 
     // record data
-    boxInput.addEventListener('keydown', (event) => {
+    boxInput.addEventListener('keydown', handleKeyDownDay);
+}
+
+/**
+ * Helper function for dayCheck and to avoid the recursive call.
+ * Recursive call would lead to fatal error of variable update.
+ * @param {*} event 
+ */
+function handleKeyDownDay(event) {
+    const boxInput = document.querySelector('#dataInput');
+    const computedStyle = window.getComputedStyle(boxInput);
+
+    // if validity check passes
+    if ((event.key==='Enter')&&(computedStyle.color==='rgb(0, 0, 0)')) {
         console.log(boxInput.value);
-        const computedStyle = window.getComputedStyle(boxInput);
-        // console.log(computedStyle.color);
-        if ((event.key==='Enter')&&(computedStyle.color==='black')) {
-        //     // TODO: record data to localstorage
-        //     const userInput = boxInput.value;
-        //     userInputArray.push(userInput);
-        //     if (userInputArray[0]) { 
-        //         // console.log(userInputArray);
-        //         localStorage.setItem('userInputs', JSON.stringify(userInputArray)); 
-        //     }
-
-            let curDialogue = document.querySelector('.dialogue-text');
-            curDialogue.textContent = 'Curious! Now, what month were you born? (enter to proceed)';
-            boxInput.value = '';
-        //     boxInput.addEventListener('input', monthCheck);
-        }
-    });
+        let curDialogue = document.querySelector('.dialogue-text');
+        curDialogue.textContent = 'Curious! Now, what month were you born? (enter to proceed)';
+        boxInput.removeEventListener('input', dayCheck);
+        boxInput.removeEventListener('keydown', handleKeyDownDay);
+        boxInput.value = '';
+        boxInput.placeholder = 'mm';
+        boxInput.addEventListener('input', monthCheck);
+    }
 }
 
+/**
+ * Check the input of month from user
+ */
 function monthCheck() {
-    // const boxInput = document.querySelector('#dataInput');
-    // boxInput.removeEventListener('input', dayCheck);
+    const boxInput = document.querySelector('#dataInput');
 
-    // // remove previous input listener style checker
-    // boxInput.removeEventListener('input', inputCheck(/^(0[1-9]|[1-2][0-9]|3[0-1])$/));
+    // remove previous input listener style checker
+    boxInput.removeEventListener('input', inputCheck(/^(0[1-9]|[1-2][0-9]|3[0-1])$/));
 
-    // // answer initialize
-    // boxInput.placeholder = 'yyyy';
+    // check validity
+    boxInput.addEventListener('input', inputCheck(/^(0[1-9]|1[0-2])$/));
 
-    // // check validity
-    // boxInput.addEventListener('input', inputCheck(/^(0[1-9]|1[0-2])$/));
-
-    // // record data
-    // boxInput.addEventListener('keydown', (event) => {
-    //     if ((event.key==='Enter')&&(boxInput.value==='black')) {
-    //         let curDialogue = document.querySelector('.dialogue-text');
-    //         curDialogue.textContent = 'Oh! Do tell me the year you were born (enter to proceed)';
-    //         boxInput.value = '';
-    //         boxInput.addEventListener('input', yearCheck);
-    //     }
-    // });
+    // record data
+    boxInput.addEventListener('keydown', handleKeyDownMonth);
 }
 
+/**
+ * Helper function for monthCheck and to avoid the recursive call.
+ * Recursive call would lead to fatal error of variable update.
+ * @param {*} event 
+ */
+function handleKeyDownMonth(event) {
+    const boxInput = document.querySelector('#dataInput');
+    const computedStyle = window.getComputedStyle(boxInput);
+
+    // if validity check passes
+    if ((event.key==='Enter')&&(computedStyle.color==='rgb(0, 0, 0)')) {
+        console.log(boxInput.value);
+        let curDialogue = document.querySelector('.dialogue-text');
+        curDialogue.textContent = 'Oh! Do tell me the year you were born (enter to proceed)';
+        boxInput.removeEventListener('input', monthCheck);
+        boxInput.removeEventListener('keydown', handleKeyDownMonth);
+        boxInput.value = '';
+        boxInput.placeholder = 'yyyy';
+        boxInput.addEventListener('input', yearCheck);
+    }
+}
+
+/**
+ * Check the input of year from user
+ */
 function yearCheck() {
     const boxInput = document.querySelector('#dataInput');
-    boxInput.removeEventListener('input', monthCheck);
 
     // remove previous input listener style checker
     boxInput.removeEventListener('input', inputCheck(/^(0[1-9]|1[0-2])$/));
-
-    // answer initialize
-    boxInput.placeholder = 'Anything';
 
     // check validity
     boxInput.addEventListener('input', inputCheck(/^(19[0-9]{2}|20[0-1][0-9]|202[0-3])$/));
 
     // record data
-    boxInput.addEventListener('keydown', (event) => {
-        if ((event.key==='Enter')&&(boxInput.value==='black')) {
-            let curDialogue = document.querySelector('.dialogue-text');
-            curDialogue.textContent = 'Finally, tell me what your current mood is';
-            boxInput.value = '';
-            boxInput.style.letterSpacing = 'normal';
-            boxInput.removeAttribute('placeholder');
-            boxInput.addEventListener('input', checkMood);
-        }
-    });
+    boxInput.addEventListener('keydown', handleKeyDownYear);
 }
 
+/**
+ * Helper function for yearCheck and to avoid the recursive call.
+ * Recursive call would lead to fatal error of variable update.
+ * @param {*} event 
+ */
+function handleKeyDownYear(event) {
+    const boxInput = document.querySelector('#dataInput');
+    const computedStyle = window.getComputedStyle(boxInput);
+
+    // if validity check passes
+    if ((event.key==='Enter')&&(computedStyle.color==='rgb(0, 0, 0)')) {
+        console.log(boxInput.value);
+        let curDialogue = document.querySelector('.dialogue-text');
+        curDialogue.textContent = 'Finally, tell me what your current mood is';
+        boxInput.removeEventListener('input', yearCheck);
+        boxInput.removeEventListener('keydown', handleKeyDownYear);
+        boxInput.value = '';
+        boxInput.placeholder = 'Anything';
+        boxInput.style.letterSpacing = 'normal';
+        boxInput.removeAttribute('placeholder');
+        boxInput.addEventListener('input', checkMood);
+    }
+}
+
+/**
+ * Check the input of mood from user
+ */
 function checkMood() {
     const boxInput = document.querySelector('#dataInput');
-    boxInput.removeEventListener('input', yearCheck);
 
     // remove previous input listener style checker
     boxInput.removeEventListener('input', inputCheck(/^(19[0-9]{2}|20[0-1][0-9]|202[0-3])$/));
@@ -151,28 +184,44 @@ function checkMood() {
     boxInput.addEventListener('input', inputCheck(/.*/));
 
     // record data
-    boxInput.addEventListener('keydown', (event) => {
-        if (event.key==='Enter') {
-            boxInput.removeEventListener('input', checkMood);
-            boxInput.hidden = true;
-            let curDialogue = document.querySelector('.dialogue-text');
-            curDialogue.textContent = '...\nThe stars have given their answer!';
-
-            // change arrow back
-            const arrowElement = document.querySelector('.arrow');
-            console.log(arrowElement);
-            const origArr = `
-                <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-                <path d="M22.5 25C18.0184 25 7.59473 12.6404 1.55317 4.96431C-0.122281 2.83559 1.72264 -0.179893 4.39835 0.243337C10.2831 1.17415 18.2164 2.28736 22.5 2.28736C26.7836 2.28736 34.7169 1.17415 40.6017 0.243339C43.2774 -0.17989 45.1223 2.83559 43.4468 4.96431C37.4053 12.6404 26.9816 25 22.5 25Z" fill="white"/>
-                </svg>
-            `;
-            arrowElement.innerHTML = origArr;
-            arrowElement.style.bottom = '0'; 
-            arrowElement.style.width = '45'; 
-            arrowElement.style.height = '25';
-        }
-    });
+    boxInput.addEventListener('keydown', handleKeyDownMood);
 }
+
+/**
+ * Helper function for checkMood and to avoid the recursive call.
+ * Recursive call would lead to fatal error of variable update.
+ * @param {*} event 
+ */
+function handleKeyDownMood(event) {
+    const boxInput = document.querySelector('#dataInput');
+    const computedStyle = window.getComputedStyle(boxInput);
+
+    // if validity check passes
+    if (event.key==='Enter') {
+        console.log(boxInput.value);
+        boxInput.hidden = true;
+        let curDialogue = document.querySelector('.dialogue-text');
+        curDialogue.textContent = 'The stars have given their answer!';
+        boxInput.removeEventListener('input', checkMood);
+        boxInput.removeEventListener('keydown', handleKeyDownMood);
+
+        // change arrow back
+        const svg = document.querySelector('.arrow');
+        const textElement = svg.querySelector('text');
+        svg.style.display = "block";
+        textElement.textContent = 'View Results!';
+        console.log(svg);
+        // const origArr = `
+        //     <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+        //     <path d="M22.5 25C18.0184 25 7.59473 12.6404 1.55317 4.96431C-0.122281 2.83559 1.72264 -0.179893 4.39835 0.243337C10.2831 1.17415 18.2164 2.28736 22.5 2.28736C26.7836 2.28736 34.7169 1.17415 40.6017 0.243339C43.2774 -0.17989 45.1223 2.83559 43.4468 4.96431C37.4053 12.6404 26.9816 25 22.5 25Z" fill="white"/>
+        //     </svg>
+        // `;
+        // arrowElement.innerHTML = origArr;
+        // arrowElement.style.bottom = '0'; 
+        // arrowElement.style.width = '45'; 
+        // arrowElement.style.height = '25';
+    }
+} 
 
 function inputCheck(pattern) {
     const boxInput = document.querySelector('#dataInput');
