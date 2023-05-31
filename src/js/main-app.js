5bb79ebcfb2b51532602a5bf3b717565a7fb3200
ruili4UCSD <rul043@ucsd.edu>
2023-05-31 16:04:01 -0700

    modifiy the css for fc list title, empty, close button

--- src/js/main-app.js
+++ src/js/main-app.js
@@ -13,26 +13,6 @@ function init() {
     // Default variables to set (i.e Celeste's name, default welcoming message etc.)
     const WELCOME_MSG = 'Hello user! What can I do for you?'
 
-    /*
-    // set those two data to local storage
-    const fortunes = [
-        {
-            "name": "Celeste",
-            "id": "fortune1",
-            "text": "you will not die soon probably"
-        },
-        {
-            "name": "Celeste",
-            "id": "fortune2",
-            "text": "you will literally explode"
-        }
-    ]
-    // Add fortune cards to local storage and HTML
-    for (let fortune of fortunes) {
-        addFortuneCard(fortune);
-    }
-    */
-
     // --------------------------START OF DIALOGUE SEQUENCE-----------------------
     // TODO: store username somewhere
     /**
@@ -44,7 +24,8 @@ function init() {
     dialogueGo(curName, WELCOME_MSG);
     // ---------------------------END OF DIALOGUE SEQUENCE------------------------
 
-    // init check-in system
+
+    // --------------------------init check-in system-----------------------
     // this fcuntion in js/util/CheckIn.js
     initializeCheckIn();
     // add event listener to check in button
@@ -55,17 +36,27 @@ function init() {
         document.getElementById('last-check-in-date').textContent = 'Last Check-In: ' + result.date;
         document.getElementById('check-in-count').textContent = 'Count: ' + result.count;
     });
+    // --------------------------end of init check-in system-----------------------
 
+
+    // --------------------------FORTUNE CARD LIST MENU-----------------------
     // FortuneCardList listeners
     const menu = document.getElementById("menu");
     const CardList = document.getElementById("CardList");
+
+
+
+
     // Add a click event listener to toggle the visibility of the content
     menu.addEventListener('click', () => {
         // check if the List is opening right now
         if (CardList.content.style.display === 'none') {
             // create List title
-            const ListTitle = document.createElement('h3');
+            const ListTitle = document.createElement('h2');
+            ListTitle.classList.add("fc-list-title");
             ListTitle.textContent = 'Saved Fortune!';
+            // Apply the styles
+            Object.assign(ListTitle.style, ListTitleStyles);
             CardList.content.appendChild(ListTitle);
             
             // Read from localstorage
@@ -73,7 +64,10 @@ function init() {
             // issue #41, if no data in fortunes, add a fortune card said no recording
             if (fortunes.length == 0){
                 const emptyFortuneCardList = document.createElement('p');
+                emptyFortuneCardList.classList.add("fc-list-empty");
                 emptyFortuneCardList.textContent = 'no content';
+                // Apply the styles
+                Object.assign(emptyFortuneCardList.style, emptyFortuneCardListStyles);
                 CardList.content.appendChild(emptyFortuneCardList);
             }
             for (let fortune of fortunes) {
@@ -85,7 +79,9 @@ function init() {
 
             // add close button on right buttom
             const closeButton = document.createElement('button');
+            closeButton.classList.add("fc-list-close-btn");
             closeButton.textContent = 'Close';
+            Object.assign(closeButton.style, closeButtonStyles);
             closeButton.addEventListener('click', e => {
                 const menu = document.getElementById("menu");
                 menu.click();
@@ -99,83 +95,8 @@ function init() {
             CardList.content.style.display = 'none';
         }
     });
-    
+    // --------------------------END OF FORTUNE CARD LIST MENU-----------------------
 }
-
-
-
-
-/**
- * Adds a button to the text box and adds an event listener for click that calls the action parameter
- * @param buttonText is a string that represents the text that is on a button
- */
-function addButtonToTextBox(buttonText) {
-    const button = document.createElement('button');
-    // Set button properties
-    button.textContent = buttonText; // Set the text content of the button
-    button.classList.add('yes-button'); // Add a CSS class for styling (optional)
-    button.disabled = false; // Set the disabled property (true or false)
-
-    // Apply button styles
-    button.style.padding = '10px 20px'; // Set padding
-    button.style.backgroundColor = 'blue'; // Set background color
-    button.style.color = 'white'; // Set text color
-    button.style.border = 'none'; // Remove border
-    button.style.cursor = 'pointer'; // Set cursor style
-
-    // Append the button to a parent element
-    const parentElement = document.querySelector(".interactive");
-    parentElement.innerHTML = '';
-    parentElement.appendChild(button);
-
-    button.addEventListener('click', async (event) => {
-        button.remove();
-        let birthday = await get_birthday();
-        let sign = await getConstellation(birthday);
-        
-        let generators = [templete1,templete2,templete3,templete4,templete5];
-
-        let reponse = await generators[Math.floor(Math.random()*generators.length)](sign);
-
-        let curDialogue = document.querySelector('.dialogue-text')
-
-        // split responses and display
-        const arrowElement = document.querySelector('.arrow');
-        await split_and_display(reponse).then(arrowElement.style.display = 'block');
-
-        // add to local storage with random id.
-        let new_fortunes = {"name": "Celeste","id": Math.floor(Math.random() * (1000 - 0 + 1)) + 0, "text": reponse};
-        addFortuneCard(new_fortunes);
-        updateFortuneCardList();
-    });
-
-}
-
-function get_birthday() {
-    return "Jan 11";
-}
-
-async function split_and_display(string) {
-    let maxLength = 100;
-    const strings = [];
-    let index = 0;
-    let curDialogue = document.querySelector('.dialogue-text')
-    while (index < string.length) {
-      const substring = string.substring(index, index + maxLength);
-      strings.push(substring);
-      index += maxLength;
-    }
-
-
-    strings.forEach((text, index) => {
-        setTimeout(() => {
-            curDialogue.textContent = text;
-          // Alternatively, you can display the text in the DOM or perform other actions
-        }, index * 5000);
-      });
-
-}
-
 
 // update the fortune card list by simply click the menu button twice
 function updateFortuneCardList(){
@@ -183,3 +104,110 @@ function updateFortuneCardList(){
     menu.click();
     menu.click();
 }
+
+
+// /**
+//  * Adds a button to the text box and adds an event listener for click that calls the action parameter
+//  * @param buttonText is a string that represents the text that is on a button
+//  */
+// function addButtonToTextBox(buttonText) {
+//     const button = document.createElement('button');
+//     // Set button properties
+//     button.textContent = buttonText; // Set the text content of the button
+//     button.classList.add('yes-button'); // Add a CSS class for styling (optional)
+//     button.disabled = false; // Set the disabled property (true or false)
+
+//     // Apply button styles
+//     button.style.padding = '10px 20px'; // Set padding
+//     button.style.backgroundColor = 'blue'; // Set background color
+//     button.style.color = 'white'; // Set text color
+//     button.style.border = 'none'; // Remove border
+//     button.style.cursor = 'pointer'; // Set cursor style
+
+//     // Append the button to a parent element
+//     const parentElement = document.querySelector(".interactive");
+//     parentElement.innerHTML = '';
+//     parentElement.appendChild(button);
+
+//     button.addEventListener('click', async (event) => {
+//         button.remove();
+//         let birthday = await get_birthday();
+//         let sign = await getConstellation(birthday);
+        
+//         let generators = [templete1,templete2,templete3,templete4,templete5];
+
+//         let reponse = await generators[Math.floor(Math.random()*generators.length)](sign);
+
+//         let curDialogue = document.querySelector('.dialogue-text')
+
+//         // split responses and display
+//         const arrowElement = document.querySelector('.arrow');
+//         await split_and_display(reponse).then(arrowElement.style.display = 'block');
+
+//         // add to local storage with random id.
+//         let new_fortunes = {"name": "Celeste","id": Math.floor(Math.random() * (1000 - 0 + 1)) + 0, "text": reponse};
+//         addFortuneCard(new_fortunes);
+//         updateFortuneCardList();
+//     });
+
+// }
+
+// function get_birthday() {
+//     return "Jan 11";
+// }
+
+// async function split_and_display(string) {
+//     let maxLength = 100;
+//     const strings = [];
+//     let index = 0;
+//     let curDialogue = document.querySelector('.dialogue-text')
+//     while (index < string.length) {
+//       const substring = string.substring(index, index + maxLength);
+//       strings.push(substring);
+//       index += maxLength;
+//     }
+
+
+//     strings.forEach((text, index) => {
+//         setTimeout(() => {
+//             curDialogue.textContent = text;
+//           // Alternatively, you can display the text in the DOM or perform other actions
+//         }, index * 5000);
+//       });
+
+// }
+
+const ListTitleStyles = {
+    // position: "absolute",
+    // marginTop: "20px",
+    // marginLeft: "20px",
+    // color: "black",
+    position: "relative",
+    // fontSize: "12px",
+    fontFamily: 'Nunito, sans-serif',
+    // backgroundColor: "#348AA7",
+    // color: "white",
+    // padding: "25px",
+    margin: "15px",
+    // borderRadius: "20px",
+    // width: "70%"
+    fontWeight: "bold",
+};
+const closeButtonStyles = {
+    position: "relative",
+    fontFamily: 'Nunito, sans-serif',
+    color: "red",
+    border: "2px solid red",
+    backgroundColor: "transparent",
+    borderRadius: "5px",
+    cursor: "pointer",
+    margin: "15px",
+    fontWeight: "bold",
+};
+const emptyFortuneCardListStyles = {
+    color: "grey",
+    position: "relative",
+    fontFamily: 'Nunito, sans-serif',
+    margin: "15px",
+    fontWeight: "bold",
+};
