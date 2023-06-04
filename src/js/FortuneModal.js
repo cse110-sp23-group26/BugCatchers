984061c5eb932f320d87beb6fb74f1c56fad67c5
ruili4UCSD <rul043@ucsd.edu>
2023-06-04 13:44:39 -0700

    modified fortune model

--- src/js/FortuneModal.js
+++ src/js/FortuneModal.js
@@ -1,23 +1,23 @@
 class FortuneModal extends HTMLElement {
     constructor() {
-        super();
-        const shadow = this.attachShadow({ mode: "open" });
-
-        const container = document.createElement("div");
-        container.setAttribute("class", "container");
-        const fortuneModalContent = document.createElement("div");
-        fortuneModalContent.setAttribute("id", "content");
-
-        const style = document.createElement("style");
-        style.innerHTML = `
+      super();
+      const shadow = this.attachShadow({ mode: "open" });
+  
+      const container = document.createElement("div");
+      container.setAttribute("class", "container");
+      const fortuneModalContent = document.createElement("div");
+      fortuneModalContent.setAttribute("id", "content");
+  
+      const style = document.createElement("style");
+      style.innerHTML = `
         html, body {
             height: 100%;
             margin: 0;
         }
-
+  
         .container {
             height: 100%;
-			width: 100%;
+            width: 100%;
             background-color: rgba(0, 0, 0, 0.6);
             display: flex;
             justify-content: center;
@@ -27,7 +27,36 @@ class FortuneModal extends HTMLElement {
             left: 0;
             z-index: 12;
         }
-
+  
+        .fc-title {
+            font-size: 36px;
+            font-weight: bold;
+            color: white;
+            text-align: center;
+        }
+        .fc-image {
+            display: block;
+            margin: 0 auto;
+            width: 150px;
+            max-width: 100%;
+            max-height: 100%;
+            object-fit: contain;
+        }
+        .fc-text {
+            text-align: center;
+        }
+        .zodiac-text {
+            text-align: center;
+            color: #BCCCDC;
+            font-style: italic;
+        }
+  
+        .fc-image-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+  
         #content {
             position: relative;
             font-size: 18px;
@@ -42,41 +71,62 @@ class FortuneModal extends HTMLElement {
             z-index: 13;
         }
         `;
-
-        container.appendChild(fortuneModalContent);
-        shadow.appendChild(style);
-        shadow.appendChild(container);
+  
+      container.appendChild(fortuneModalContent);
+      shadow.appendChild(style);
+      shadow.appendChild(container);
     }
-
+  
     /**
      * Passed in as JSON format as follows:
      * 
      * {
-     *      "name": ...
-     *      "id": ...
-     *      "text": ...
+     *      "name": ...string
+     *      "id": ...num
+     *      "text": ...string
+     *      "birthday": ...string
+     *      "mood": ...string
+     *      "time": ...string
+     *      "modal-text": ... (only displayed when fortune is clicked)
      * }
      */
     set data(data) {
-        if (!data) return;
-
-        const fortuneCardContent = this.shadowRoot.getElementById("content");
-        const container = this.shadowRoot.querySelector(".container");
-
-        const textParagraph = document.createElement("p");
-        textParagraph.classList.add("fc-text");
-        textParagraph.textContent = data.text;
-
-        const nameParagraph = document.createElement("p");
-        nameParagraph.classList.add("fc-name");
-        nameParagraph.textContent = data.name;
-
-        fortuneCardContent.append(textParagraph, nameParagraph);
-
-        container.addEventListener("click", () => {
-            this.remove();
-        });
+      if (!data) return;
+  
+      const fortuneCardContent = this.shadowRoot.getElementById("content");
+      const container = this.shadowRoot.querySelector(".container");
+      // zodiac title
+      const title = document.createElement("h1");
+      title.classList.add("fc-title");
+      title.textContent = data.name;
+      // fortune response
+      const textParagraph = document.createElement("p");
+      textParagraph.classList.add("fc-text");
+      textParagraph.textContent = data.text;
+      // zodiac img
+      const image = document.createElement("img");
+      image.src = `assets/constellation/big/white/${data.name}.png`;
+      image.classList.add("fc-image");
+  
+      const zodiacInfo = getConstellationInfo(data.name);
+      // zodiacInfo[0]: zodiac info
+      // zodiacInfo[1]: zodiac color
+      const zodiacParagraph = document.createElement("p");
+      zodiacParagraph.classList.add("zodiac-text");
+      zodiacParagraph.textContent = zodiacInfo[0];
+  
+      const imageContainer = document.createElement("div");
+      imageContainer.classList.add("fc-image-container");
+      imageContainer.appendChild(image);
+  
+      fortuneCardContent.append(title, textParagraph, imageContainer, zodiacParagraph);
+      fortuneCardContent.style.backgroundColor = zodiacInfo[1];
+  
+      container.addEventListener("click", () => {
+        this.remove();
+      });
     }
-}
-
-customElements.define("fortune-modal", FortuneModal);
+  }
+  
+  customElements.define("fortune-modal", FortuneModal);
+  
---
+++ src/js/util/GetConstellationColor.js
@@ -0,0 +1,61 @@
+// Given a string representing a constellation (such as Pisces), return two values.
+// The first value is a string, which is an introduction to this constellation.
+// The second value is a value representing the color available for JS, 
+// which is more suitable for the given constellation and is dark. 
+// This dark color can be used as the background color, because our text is all white.
+
+
+function getConstellationInfo(constellation) {
+    const constellationData = {
+      "Pisces": [
+        "Pisces, a water sign, is the last constellation of the zodiac. It's symbolized by two fish swimming in opposite directions, representing the constant division of Pisces's attention between fantasy and reality.",
+        "#264653"
+      ],
+      "Aries": [
+        "Aries is the first sign of the zodiac, and that’s pretty much how those born under this sign see themselves: first. Aries are the leaders of the pack, first in line to get things going.",
+        "#2a9d8f"
+      ],
+      "Taurus": [
+        "Taurus, the second sign of the zodiac and the ruler of the earth sign, is all about reward. Unlike the Aries love of the game, Taurus loves the rewards of the game.",
+        "#e76f51"
+      ],
+      "Gemini": [
+        "Gemini is the third sign of the zodiac, and those born under this sign will be quick to tell you all about it. That’s because they love to talk!",
+        "#e9c46a"
+      ],
+      "Cancer": [
+        "Cancer, the fourth sign of the zodiac, is all about home. Those born under this horoscope sign are ‘roots’ kinds of people, and take great pleasure in the comforts of home and family.",
+        "#f4a261"
+      ],
+      "Leo": [
+        "Roll out the red carpet, because Leo has arrived. Leo is represented by the lion, and these spirited fire signs are the kings and queens of the celestial jungle.",
+        "#e76f51"
+      ],
+      "Virgo": [
+        "Virgo is an earth sign historically represented by the goddess of wheat and agriculture, an association that speaks to Virgo’s deep-rooted presence in the material world.",
+        "#264653"
+      ],
+      "Libra": [
+        "Libra is an air sign represented by the scales (interestingly, the only inanimate object of the zodiac), an association that reflects Libra's fixation on balance and harmony.",
+        "#2a9d8f"
+      ],
+      "Scorpio": [
+        "Scorpio is one of the most misunderstood signs of the zodiac. Because of its incredible passion and power, Scorpio is often mistaken for a fire sign.",
+        "#e9c46a"
+      ],
+      "Sagittarius": [
+        "Sagittarius, the ninth sign of the zodiac, is the home of the wanderers of the zodiac. It’s not a mindless ramble for these folks, either. Sagittarians are truth-seekers, and the best way for them to do this is to hit the road, talk to others and get some answers.",
+        "#f4a261"
+      ],
+      "Capricorn": [
+        "Capricorn, the tenth sign of the zodiac, is all about hard work. Those born under this sign are more than happy to put in a full day at the office, realizing that it will likely take a lot of those days to get to the top.",
+        "#e76f51"
+      ],
+      "Aquarius": [
+        "Aquarius is the eleventh sign of the zodiac, and Aquarians are the perfect representatives for the Age of Aquarius. Those born under this horoscope sign have the social conscience needed to carry us into the new millennium.",
+        "#264653"
+      ]
+    };
+  
+    return constellationData[constellation] || ["Unknown constellation.", "#ffffff"];
+  }
