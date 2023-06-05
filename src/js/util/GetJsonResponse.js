feb8c5809da7603a5773def73686c76af2d0dff2
Shishir Iyer <shiyer@ucsd.edu>
2023-06-04 20:18:12 -0700

    i think i changed everything i needed to??

--- src/js/util/GetJsonResponse.js
+++ src/js/util/GetJsonResponse.js
@@ -1,9 +1,8 @@
 // return the resonpse based on the five input value
 async function GetJsonResponse(UserConstellation, UserBirthDay, UserBirthMonth, UserBirthYear, UserMood){
     //return `Your constellation is ${UserConstellation}, your date of birth is ${UserBirthYear}-${UserBirthMonth}-${UserBirthDay}, your current mood is ${UserMood}`;
-    let generators = await [templete1,templete2,templete3,templete4,templete5];
-    let generator = await generators[Math.floor(Math.random()*generators.length)];
+    let generators = [templete1,templete2,templete3,templete4,templete5];
+    let generator = generators[Math.floor(Math.random()*generators.length)];
     let response = await generator(UserConstellation);
     return response;
-    
 }
