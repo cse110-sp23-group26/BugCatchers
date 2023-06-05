b2b56e5ef7c32ec93fb6c2c3b36e91985765f896
Shishir Iyer <shiyer@ucsd.edu>
2023-06-04 21:16:23 -0700

    fixed missing template issue by getting rid of them (we can add them back in later)

--- src/js/util/GetJsonResponse.js
+++ src/js/util/GetJsonResponse.js
@@ -1,7 +1,7 @@
 // return the resonpse based on the five input value
 async function GetJsonResponse(UserConstellation, UserBirthDay, UserBirthMonth, UserBirthYear, UserMood){
     //return `Your constellation is ${UserConstellation}, your date of birth is ${UserBirthYear}-${UserBirthMonth}-${UserBirthDay}, your current mood is ${UserMood}`;
-    let generators = [templete1,templete2,templete3,templete4,templete5];
+    let generators = [templete1,templete2,templete3];
     let generator = generators[Math.floor(Math.random()*generators.length)];
     let response = await generator(UserConstellation);
     return response;
