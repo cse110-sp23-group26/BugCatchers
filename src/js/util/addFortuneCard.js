65a4e58a1e76d5e712301ef3501888750967456d
Ryan McCrystal <rrmccrystal@gmail.com>
2023-06-11 19:32:17 -0700

    feat: Export addFortuneCard

--- src/js/util/addFortuneCard.js
+++ src/js/util/addFortuneCard.js
@@ -5,7 +5,7 @@
  */
 // Then add it to localStorage (only add when this fortune card does not exist in localstorage)
 // Then append it to HTMLAllCollection, under fortuneCardList 
-function addFortuneCard(fortune) {
+export function addFortuneCard(fortune) {
 	// Add to local storage
 	let fortunes = JSON.parse(localStorage.getItem('FortunesCard')) || [];
 	let existingFortune = fortunes.find(f => f.id === fortune.id);
