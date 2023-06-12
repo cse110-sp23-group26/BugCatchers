dda4a1ab9e28162321dc1fe3d8512c4f5e8a289d
ruili4UCSD <rul043@ucsd.edu>
2023-06-11 22:17:44 -0700

    constellationAnimation.js

--- tests/Animation.test.js
+++ tests/Animation.test.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const showConstellationImage = require("../src/js/util/constellationAnimation.js");
+// const showConstellationImage = require("../src/js/util/constellationAnimation.js");
 
 describe('Animation for showConstellationImage', () => {
   let browser;
@@ -32,3 +32,48 @@ describe('Animation for showConstellationImage', () => {
     expect(overlayOpacity).toBe(NaN);
   });
 });
+
+function showConstellationImage(constellation) {
+	// add the image
+	const image = document.createElement("img");
+	image.src = `assets/constellation/big/white/${constellation}.png`;
+	image.style.position = "fixed";
+	image.style.top = "40%";
+	image.style.left = "50%";
+	image.style.transform = "translate(-50%, -50%)";
+	image.style.opacity = "0";
+	image.style.transition = "opacity 2s ease-in-out";
+
+	// add the overlay
+	const overlay = document.createElement("div");
+	overlay.style.position = "fixed";
+	overlay.style.top = "0";
+	overlay.style.left = "0";
+	overlay.style.width = "100%";
+	overlay.style.height = "100%";
+	overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+	overlay.style.opacity = "0";
+	overlay.style.transition = "opacity 2s ease-in-out";
+
+	document.body.appendChild(overlay);
+	document.body.appendChild(image);
+
+	setTimeout(() => {
+		overlay.style.opacity = "0.5";
+		image.style.opacity = "1";
+	}, 0);
+
+	const totalDuration = 5000; // 整个过程的持续时间（毫秒）- Duration of the entire process (milliseconds)
+	const startFadeOutTime = totalDuration - 2000; // 图片淡出开始的时间（毫秒）- The time at which the image fade starts (in milliseconds)
+
+	setTimeout(() => {
+		image.style.opacity = "0";
+		overlay.style.opacity = "0";
+	}, startFadeOutTime);
+
+	// remove the img and overlay
+	setTimeout(() => {
+		overlay.remove();
+		image.remove();
+	}, totalDuration);
+}
